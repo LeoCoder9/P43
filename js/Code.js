e11b62224599ce78cf4877fc76a3e54a71d3fa98
LeoCoder9 <76700971+LeoCoder9@users.noreply.github.com>
2021-07-05 17:52:16 +0100

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,28 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+      
+fill("white")
+textSize(15)
+text("B A T S A D A E", 200, 320)
+fill("lightblue")
+text("Hint: Store data!!!", 200,340)
+
+
+      
+fill("white")
+textSize(15)
+text("C N F T U O N I ", 700, 160)
+fill("lightblue")
+text("Hint: Performs the instruction told!!!!!!", 700,180)
+
+}
